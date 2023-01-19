8324eb6e6144ab1ecf2d2add6098853016ac28d6
Nir Nussbaum <22nn221@gmail.com>
2023-01-19 09:58:13 +0200

    Fix bug .cmp

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -9,16 +9,19 @@ def jack_analyzer(input_path: str):
         for file_name in os.listdir(input_path) :
             file_path = os.path.join(input_path, file_name)
             # checking if it is a file
-            if os.path.isfile(file_path):
+            if os.path.isfile(file_path) and file_path.rfind(".jack") != -1:
                 output_path_file = str(file_path).replace(".jack", ".xml")
                 engine = CompilationEngine(file_path, output_path_file) 
                 engine.compile_class()
 
     #One single file.    
-    elif os.path.isfile(input_path): 
+    elif os.path.isfile(input_path) and input_path.rfind(".jack") != -1: 
         output_path_file = str(input_path).replace(".jack", ".xml")
         engine = CompilationEngine(input_path, output_path_file) 
         engine.compile_class()
 
-if __name__ == "__main__" and len(sys.argv) == 2:
-    jack_analyzer(sys.argv[1])
+# if __name__ == "__main__" and len(sys.argv) == 2:
+#     jack_analyzer(sys.argv[1])
+
+if __name__ == "__main__":
+    jack_analyzer("./text.cmp")
